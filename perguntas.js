149de3bfb3ebdb5df7294cbc9651bd299df80f2d
mariaclara05112007 <138144874+mariaclara05112007@users.noreply.github.com>
2024-09-30 13:57:28 -0300

    30/09

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Oque é Python?',
+    'OPython é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'qual a capital da França',
+    'A capital da frança é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função',
+    'Uma função é um bloco de código que alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
